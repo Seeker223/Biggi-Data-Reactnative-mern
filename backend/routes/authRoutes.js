25ee0af06ce12aeb4758d20d4225176c9c95c879
BADUN OLUWATOBI <badunoluwatobi2024@gmail.com>
2025-11-13 15:57:25 +0100

    updated authController, UserModel, authRoutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,14 +5,13 @@ import {
   verifySecurityPin,
   resendSecurityPin,
 } from "../controllers/authController.js";
-import { testEmail } from "../controllers/testAuthController.js";
 
 const router = express.Router();
 
+// 🔐 Auth routes
 router.post("/register", register);
 router.post("/login", login);
 router.post("/verify-pin", verifySecurityPin);
 router.post("/resend-pin", resendSecurityPin);
-router.post("/test-email", testEmail) 
 
 export default router;
