ac49b4d7b39742d7df3c99655c8c2c4584d1489e
BADUN OLUWATOBI <badunoluwatobi2024@gmail.com>
2025-11-17 15:34:38 +0100

    walletController withdraw without admin approval

--- backend/models/withdrawModel.js
+++ backend/models/withdrawModel.js
@@ -2,24 +2,26 @@ import mongoose from "mongoose";
 
 const withdrawSchema = new mongoose.Schema(
   {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    method: {
-      type: String,
-      enum: ["Opay", "Bank Transfer"],
-      required: true,
-    },
-    bank: { type: String },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+
+    method: { type: String, enum: ["Opay", "Bank Transfer"], required: true },
+    bank: { type: String, default: "Opay" },
     accountNumber: { type: String, required: true },
     accountName: { type: String, required: true },
+
     amount: { type: Number, required: true },
+
     status: {
       type: String,
-      enum: ["pending", "completed", "failed"],
+      enum: ["pending", "approved", "rejected"],
       default: "pending",
+    },
+
+    adminNote: { type: String, default: "" },
+    approvedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
     },
   },
   { timestamps: true }
